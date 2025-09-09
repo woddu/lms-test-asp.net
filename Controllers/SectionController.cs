175757bace05f7492a804e08887d03a7ad869adc
woddu <fwoddu@yahoo.com>
2025-09-09 20:06:00 +0800

    section scaffold

---
+++ Controllers/SectionController.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using lms_test1.Data;
+using lms_test1.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace lms_test1.Controllers;
+[Authorize(Roles = "Admin,HeadTeacher")]
+public class SectionController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public SectionController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Section
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Sections.ToListAsync());
+    }
+
+    // GET: Section/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var section = await _context.Sections
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (section == null)
+        {
+            return NotFound();
+        }
+
+        return View(section);
+    }
+
+    // GET: Section/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Section/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Name,Track,YearLevel")] Section section)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(section);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(section);
+    }
+
+    // GET: Section/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var section = await _context.Sections.FindAsync(id);
+        if (section == null)
+        {
+            return NotFound();
+        }
+        return View(section);
+    }
+
+    // POST: Section/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Track,YearLevel")] Section section)
+    {
+        if (id != section.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(section);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SectionExists(section.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(section);
+    }
+
+    // GET: Section/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var section = await _context.Sections
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (section == null)
+        {
+            return NotFound();
+        }
+
+        return View(section);
+    }
+
+    // POST: Section/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var section = await _context.Sections.FindAsync(id);
+        if (section != null)
+        {
+            _context.Sections.Remove(section);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool SectionExists(int id)
+    {
+        return _context.Sections.Any(e => e.Id == id);
+    }
+}
+
