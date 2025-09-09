bec5b72e092f309f9778eb3adf18d3fdaf4d12f0
woddu <fwoddu@yahoo.com>
2025-09-09 20:22:00 +0800

    added viewmodel for users with rules for teachers management

---
+++ Models/ViewModels/Teachers/UserWithRolesViewModel.cs
@@ -0,0 +1,9 @@
+namespace lms_test1.Models.ViewModels.Teachers;
+
+public class UserWithRolesViewModel
+{
+    public required string Id { get; set; }
+    public required string Initials { get; set; }
+    public required bool Verified { get; set; }
+    public required string Role { get; set; }
+}
