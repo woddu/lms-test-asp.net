a0266c55ede6cf52d40cafbae5801ae0c033f13b
woddu <fwoddu@yahoo.com>
2025-09-10 23:02:32 +0800

    implemented the seeder

--- Program.cs
+++ Program.cs
@@ -60,6 +60,9 @@ using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     await DbSeeder.SeedRolesAndUsersAsync(services);
+
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    await DbSeeder.SeedEntitiesAsync(context);
 }
 
 
