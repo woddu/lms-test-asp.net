6789c3dc99081ec613c1ced05475350be27907d6
woddu <fwoddu@yahoo.com>
2025-09-10 21:00:46 +0800

    added more view models for teachers controller and views

--- Models/ViewModels/Teachers/LMSUserDetailVewModel.cs
+++ Models/ViewModels/Teachers/LMSUserDetailVewModel.cs
@@ -1,17 +1,14 @@
 using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace lms_test1.Models.ViewModels.Teachers;
 
-public class LMSUserEditViewModel
+public class LMSUserDetailViewModel
 {
     public string Id { get; set; } = string.Empty;
 
-    [Required]
     [Display(Name = "First Name")]
     public string FirstName { get; set; } = string.Empty;
 
-    [Required]
     [Display(Name = "Last Name")]
     public string LastName { get; set; } = string.Empty;
     
@@ -28,8 +25,10 @@ public class LMSUserEditViewModel
     [Display(Name = "User Name")]
     public string UserName { get; set; } = string.Empty;
 
-    public List<SelectListItem>? AdvisorySectionOptions { get; set; }
+    public Section? AdvisorySection { get; set; }
 
     // Optional: for display only, not editable
-    public List<TeacherSubjectViewModel>? Subjects { get; set; }
+    public ICollection<Subject>? Subjects { get; set; }
+
+    
 }
---
+++ Models/ViewModels/Teachers/TeacherSubjectViewModel.cs
@@ -0,0 +1,7 @@
+namespace lms_test1.Models.ViewModels.Teachers;
+
+public class TeacherSubjectViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
