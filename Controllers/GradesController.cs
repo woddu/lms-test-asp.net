14613d4551daeea851b2c95dac9d0ad4186693b1
woddu <fwoddu@yahoo.com>
2025-09-25 13:29:07 +0800

    used the section with students dto and added search students api

--- Controllers/GradesController.cs
+++ Controllers/GradesController.cs
@@ -1,6 +1,8 @@
 
 using System.Security.Claims;
 using lms_test1.Data;
+using lms_test1.Models.DTO.Student;
+using lms_test1.Models.DTO.Section;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -33,19 +35,40 @@ public class GradesController : Controller
     public async Task<IActionResult> SectionStudents(int id)
     {
         var section = await _context.Sections
-            .Include(s => s.Students)
             .FirstOrDefaultAsync(s => s.Id == id);
-
+        
         if (section == null)
         {
             return NotFound();
         }
 
-        return View(section);
+        // trim the student data to include only necessary information
+        var students = await _context.Students
+            .OrderBy(st => st.Gender)
+            .ThenBy(st => st.LastName)
+            .ThenBy(st => st.FirstName)
+            .Where(st => st.SectionId == id)
+            .Select(st => new StudentInListDTO
+            (
+                st.Id,
+                st.FirstName,
+                st.LastName,
+                st.Gender,
+                st.MiddleName
+            ))
+            .ToListAsync();
+
+        var dto = new SectionWithStudentsDTO
+        (
+            Section: section,
+            Students: students
+        );
+
+        return View(dto);
     }
 
     public async Task<IActionResult> StudentGrades(int id)
-    {
+    {        
         var student = await _context.Students
             .Include(st => st.Scores!)
                 .ThenInclude(sc => sc.TeacherSubject)
@@ -58,5 +81,30 @@ public class GradesController : Controller
         }
 
         return View(student);
+    }
+
+    public async Task<IActionResult> SearchStudent(string searchTerm, int sectionId)
+    {
+        if (string.IsNullOrWhiteSpace(searchTerm))
+        {
+            return Json(new { results = new List<object>() });
+        }
+
+        var students = await _context.Students
+            .Where(st => st.SectionId == sectionId &&
+                        (st.LastName.ToLower().Contains(searchTerm.ToLower()) ||
+                         st.FirstName.ToLower().Contains(searchTerm.ToLower())
+                        ))
+            .Select(st => new StudentInListDTO
+            (
+                st.Id,
+                st.FirstName,
+                st.LastName,
+                st.Gender,
+                st.MiddleName
+            ))
+            .ToListAsync();
+
+        return PartialView("_StudentTableRows", students);
     }
 }
