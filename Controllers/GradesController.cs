9ba4050133961c05aec1d233c62034f6484e27c6
woddu <fwoddu@yahoo.com>
2025-09-27 22:35:09 +0800

    added twoendpoints for saving scores of subject and student, and added authorization service

--- Controllers/GradesController.cs
+++ Controllers/GradesController.cs
@@ -8,6 +8,7 @@ using lms_test1.Models.DTO.TeacherSubject;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using lms_test1.Models.DTO.Score;
 
 namespace lms_test1.Controllers;
 
@@ -16,9 +17,12 @@ public class GradesController : Controller
 {
     private readonly ApplicationDbContext _context;
 
-    public GradesController(ApplicationDbContext context)
+    private readonly IAuthorizationService _authorizationService;
+
+    public GradesController(ApplicationDbContext context, IAuthorizationService authorizationService)
     {
         _context = context;
+        _authorizationService = authorizationService;
     }
 
     //Get all the teacher's subjects
@@ -46,6 +50,48 @@ public class GradesController : Controller
         if (teacherSubject == null) return NotFound();
 
         return View(teacherSubject);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> SaveSubjectScoresChanges([FromBody] TeacherSubjectDTO model)
+    {
+        if (!ModelState.IsValid)
+        {
+            var errors = ModelState
+            .Where(ms => ms.Value!.Errors.Count > 0)
+            .Select(ms => new
+            {
+                Field = ms.Key,
+                Errors = ms.Value!.Errors.Select(e => e.ErrorMessage).ToArray()
+            });
+            return BadRequest(errors);
+        }
+
+        var ts = await _context.TeacherSubjects
+            .Where(ts => ts.Id == model.Id)
+            .FirstOrDefaultAsync();
+
+        if (ts == null) return NotFound();
+
+        var result = await _authorizationService.AuthorizeAsync(User, ts, "SameOwnerOfTeacherSubject");
+        if (!result.Succeeded) return Forbid();
+
+        ApplyChanges(model, ts, _context);
+
+        await _context.SaveChangesAsync();
+
+        var updatedTs = await _context.TeacherSubjects
+            .Where(t => t.Id == ts.Id)
+            .FirstOrDefaultAsync();
+
+        if (updatedTs == null) return NotFound();
+
+        return Json(new { 
+            success = true, 
+            redirectUrl = Url.Action("SubjectDetails", "Grades", new { teacherSubjectId = updatedTs.Id }) 
+        });
+
     }
 
     //Get the students in a section for a specific teacher's subject
@@ -135,7 +181,7 @@ public class GradesController : Controller
                 StudentScore = newScore
             };
             return View(viewModel);
-            
+
         }
         else
         {
@@ -157,6 +203,50 @@ public class GradesController : Controller
             };
             return View(viewModel);
         }
+
+    }
+
+    public async Task<IActionResult> SaveStudentScoresChanges([FromBody] ScoreDTO model)
+    {
+        if (!ModelState.IsValid)
+        {
+            var errors = ModelState
+            .Where(ms => ms.Value!.Errors.Count > 0)
+            .Select(ms => new
+            {
+                Field = ms.Key,
+                Errors = ms.Value!.Errors.Select(e => e.ErrorMessage).ToArray()
+            });
+            return BadRequest(errors);
+        }
+
+        var score = await _context.Scores
+            .Include(s => s.TeacherSubject)
+            .Where(s => s.Id == model.Id)
+            .FirstOrDefaultAsync();
+
+        if (score == null) return NotFound();
+        
+        var result = await _authorizationService.AuthorizeAsync(User, score.TeacherSubject, "SameOwnerOfTeacherSubject");
+        if (!result.Succeeded) return Forbid();
+
+        ApplyChanges(model, score, _context);
+
+        await _context.SaveChangesAsync();
+
+        var updatedScore = await _context.Scores
+            .Include(s => s.TeacherSubject)
+            .Include(s => s.Student)
+            .Where(s => s.Id == score.Id)
+            .FirstOrDefaultAsync();
+
+        if (updatedScore == null) return NotFound();
+
+        return Json(new
+        {
+            success = true,
+            redirectUrl = Url.Action("StudentScores", "Grades", new { studentId = updatedScore.StudentId, teacherSubjectId = updatedScore.TeacherSubjectId })
+        });
 
     }
 
@@ -188,5 +278,29 @@ public class GradesController : Controller
             .ToList();
 
         return PartialView("_StudentTableRows", students);
+
     }
+    
+    public static void ApplyChanges<TSource, TDest>(TSource source, TDest dest, DbContext ctx)
+    {
+        var srcProps = typeof(TSource).GetProperties();
+        var destProps = typeof(TDest).GetProperties().ToDictionary(p => p.Name);
+
+        var entry = ctx.Entry(dest!);
+
+        foreach (var sp in srcProps)
+        {
+            if (!destProps.TryGetValue(sp.Name, out var dp)) continue;
+
+            var newValue = sp.GetValue(source);
+            var oldValue = dp.GetValue(dest);
+
+            if (!Equals(newValue, oldValue))
+            {
+                dp.SetValue(dest, newValue);
+                entry.Property(dp.Name).IsModified = true;
+            }
+        }
+    }
+
 }
