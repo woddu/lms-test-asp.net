ba4249308841e18cc40a52dab466c5056c5786d9
woddu <fwoddu@yahoo.com>
2025-09-29 15:20:22 +0800

    migration for LMSUser Many Section relation

---
+++ Data/Migrations/20250929051802_ChangeLMSUserAdvisoryToAdvisories.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lms_test1.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeLMSUserAdvisoryToAdvisories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Sections_AdviserId",
+                table: "Sections");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sections_AdviserId",
+                table: "Sections",
+                column: "AdviserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Sections_AdviserId",
+                table: "Sections");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sections_AdviserId",
+                table: "Sections",
+                column: "AdviserId",
+                unique: true);
+        }
+    }
+}
