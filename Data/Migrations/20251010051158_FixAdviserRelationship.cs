ee0266fc3cd08831e43c6043df82242335b42bc0
woddu <fwoddu@yahoo.com>
2025-10-10 20:59:24 +0800

    added migrations for manually setting up section and lmsuser relationship

---
+++ Data/Migrations/20251010051158_FixAdviserRelationship.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lms_test1.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAdviserRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sections_AspNetUsers_AdviserId",
+                table: "Sections");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sections_AspNetUsers_AdviserId",
+                table: "Sections",
+                column: "AdviserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sections_AspNetUsers_AdviserId",
+                table: "Sections");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sections_AspNetUsers_AdviserId",
+                table: "Sections",
+                column: "AdviserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
