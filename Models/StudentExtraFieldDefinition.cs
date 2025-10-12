91baad2f1440b22869493828d0d0b0cc5581c899
woddu <fwoddu@yahoo.com>
2025-10-12 20:49:52 +0800

    add a new model for definitions of extrafields

---
+++ Models/StudentExtraFieldDefinition.cs
@@ -0,0 +1,8 @@
+namespace lms_test1.Models;
+
+public class StudentExtraFieldDefinition
+{
+    public int Id { get; set; }
+    public required string FieldName { get; set; }
+    public required string FieldType { get; set; }
+}
