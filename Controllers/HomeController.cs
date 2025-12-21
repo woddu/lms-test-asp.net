2117743a6c12e9016849703314abb4c023a61b6b
woddu <fwoddu@yahoo.com>
2025-12-21 14:13:31 +0800

    refactor: optimize Index method in HomeController for better performance and readability

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,8 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using lms_test1.Models;
 using Microsoft.AspNetCore.Authorization;
+using lms_test1.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace lms_test1.Controllers;
 
@@ -9,17 +11,24 @@ namespace lms_test1.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
-    {
-        return View();
+    public async Task<IActionResult> Index()
+    {        
+        var announcements = await _context.Announcements
+            .OrderByDescending(a => a.DatePosted)
+            .ToListAsync();
+
+        return View(announcements);
     }
 
+    [Authorize(Roles = "Admin")]
     public IActionResult Privacy()
     {
         return View();
